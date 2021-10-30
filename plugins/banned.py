397124f09078cb6c140da6884a87d94854d914bd
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-10-30 08:38:40 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -25,7 +25,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('Support', url='https://t.me/EvaMariaSupport')
+        InlineKeyboardButton('Support', url='t.me/Mo_Tech_yT')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)

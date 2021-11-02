6d8d46c3978a75a064b257f851e19c6fd874c97b
MRK-YT <81806065+MRK-YT@users.noreply.github.com>
2021-11-02 23:23:02 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,6 +1,7 @@
 import os
 from pyrogram import Client, filters
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
+
 from utils import extract_user, get_file_id, get_poster, last_online
 import time
 from datetime import datetime
@@ -29,15 +30,15 @@ async def showid(client, message):
         if message.reply_to_message:
             _id += (
                 "<b>➲ User ID</b>: "
-                f"<code>{message.from_user.id}</code>\n"
+                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
                 "<b>➲ Replied User ID</b>: "
-                f"<code>{message.reply_to_message.from_user.id}</code>\n"
+                f"<code>{message.reply_to_message.from_user.id if message.reply_to_message.from_user else 'Anonymous'}</code>\n"
             )
             file_info = get_file_id(message.reply_to_message)
         else:
             _id += (
                 "<b>➲ User ID</b>: "
-                f"<code>{message.from_user.id}</code>\n"
+                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
             )
             file_info = get_file_id(message)
         if file_info:
@@ -67,62 +68,61 @@ async def who_is(client, message):
         await status_message.edit(str(error))
         return
     if from_user is None:
-        await status_message.edit("no valid user_id / message specified")
+        return await status_message.edit("no valid user_id / message specified")
+    message_out_str = ""
+    message_out_str += f"<b>➲First Name:</b> {from_user.first_name}\n"
+    last_name = from_user.last_name or "<b>None</b>"
+    message_out_str += f"<b>➲Last Name:</b> {last_name}\n"
+    message_out_str += f"<b>➲Telegram ID:</b> <code>{from_user.id}</code>\n"
+    username = from_user.username or "<b>None</b>"
+    dc_id = from_user.dc_id or "[User Doesnt Have A Valid DP]"
+    message_out_str += f"<b>➲Data Centre:</b> <code>{dc_id}</code>\n"
+    message_out_str += f"<b>➲User Name:</b> @{username}\n"
+    message_out_str += f"<b>➲User 𝖫𝗂𝗇𝗄:</b> <a href='tg://user?id={from_user.id}'><b>Click Here</b></a>\n"
+    if message.chat.type in (("supergroup", "channel")):
+        try:
+            chat_member_p = await message.chat.get_member(from_user.id)
+            joined_date = datetime.fromtimestamp(
+                chat_member_p.joined_date or time.time()
+            ).strftime("%Y.%m.%d %H:%M:%S")
+            message_out_str += (
+                "<b>➲Joined this Chat on:</b> <code>"
+                f"{joined_date}"
+                "</code>\n"
+            )
+        except UserNotParticipant:
+            pass
+    chat_photo = from_user.photo
+    if chat_photo:
+        local_user_photo = await client.download_media(
+            message=chat_photo.big_file_id
+        )
+        buttons = [[
+            InlineKeyboardButton('🔐 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_photo(
+            photo=local_user_photo,
+            quote=True,
+            reply_markup=reply_markup,
+            caption=message_out_str,
+            parse_mode="html",
+            disable_notification=True
+        )
+        os.remove(local_user_photo)
     else:
-        message_out_str = ""
-        message_out_str += f"<b>➲First Name:</b> {from_user.first_name}\n"
-        last_name = from_user.last_name or "<b>None</b>"
-        message_out_str += f"<b>➲Last Name:</b> {last_name}\n"
-        message_out_str += f"<b>➲Telegram ID:</b> <code>{from_user.id}</code>\n"
-        username = from_user.username or "<b>None</b>"
-        dc_id = from_user.dc_id or "[User Doesnt Have A Valid DP]"
-        message_out_str += f"<b>➲Data Centre:</b> <code>{dc_id}</code>\n"
-        message_out_str += f"<b>➲User Name:</b> @{username}\n"
-        message_out_str += f"<b>➲User 𝖫𝗂𝗇𝗄:</b> <a href='tg://user?id={from_user.id}'><b>Click Here</b></a>\n"
-        if message.chat.type in (("supergroup", "channel")):
-            try:
-                chat_member_p = await message.chat.get_member(from_user.id)
-                joined_date = datetime.fromtimestamp(
-                    chat_member_p.joined_date or time.time()
-                ).strftime("%Y.%m.%d %H:%M:%S")
-                message_out_str += (
-                    "<b>➲Joined this Chat on:</b> <code>"
-                    f"{joined_date}"
-                    "</code>\n"
-                )
-            except UserNotParticipant:
-                pass
-        chat_photo = from_user.photo
-        if chat_photo:
-            local_user_photo = await client.download_media(
-                message=chat_photo.big_file_id
-            )
-            buttons = [[
-                InlineKeyboardButton('🔐 Close', callback_data='close_data')
-            ]]
-            reply_markup = InlineKeyboardMarkup(buttons)
-            await message.reply_photo(
-                photo=local_user_photo,
-                quote=True,
-                reply_markup=reply_markup,
-                caption=message_out_str,
-                parse_mode="html",
-                disable_notification=True
-            )
-            os.remove(local_user_photo)
-        else:
-            buttons = [[
-                InlineKeyboardButton('🔐 Close', callback_data='close_data')
-            ]]
-            reply_markup = InlineKeyboardMarkup(buttons)
-            await message.reply_text(
-                text=message_out_str,
-                reply_markup=reply_markup,
-                quote=True,
-                parse_mode="html",
-                disable_notification=True
-            )
-        await status_message.delete()
+        buttons = [[
+            InlineKeyboardButton('🔐 Close', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            text=message_out_str,
+            reply_markup=reply_markup,
+            quote=True,
+            parse_mode="html",
+            disable_notification=True
+        )
+    await status_message.delete()
 
 @Client.on_message(filters.command(["imdb", 'search']))
 async def imdb_search(client, message):
@@ -152,17 +152,23 @@ async def imdb_callback(bot: Client, query: CallbackQuery):
     btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{imdb.get('title')} - {imdb.get('year')}",
+                    text=f"{imdb.get('title')}",
                     url=imdb['url'],
                 )
             ]
         ]
     if imdb.get('poster'):
-        await query.message.reply_photo(photo=imdb['poster'], caption=f"IMDb Data:\n\n🏷 Title:<a href={imdb['url']}>{imdb.get('title')}</a>\n🎭 Genres: {imdb.get('genres')}\n📆 Year:<a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n🖋 StoryLine: <code>{imdb.get('plot')} </code>", reply_markup=InlineKeyboardMarkup(btn))
+        try:
+            await query.message.reply_photo(photo=imdb['poster'], caption=f"IMDb Data:\n\n🏷 Title:<a href={imdb['url']}>{imdb.get('title')}</a>\n🎭 Genres: {imdb.get('genres')}\n📆 Year:<a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n🖋 StoryLine: <code>{imdb.get('plot')} </code>", reply_markup=InlineKeyboardMarkup(btn))
+        except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
+            pic = imdb.get('poster')
+            poster = pic.replace('.jpg', "._V1_UX360.jpg")
+            await query.message.reply_photo(photo=imdb['poster'], caption=f"IMDb Data:\n\n🏷 Title:<a href={imdb['url']}>{imdb.get('title')}</a>\n🎭 Genres: {imdb.get('genres')}\n📆 Year:<a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n🖋 StoryLine: <code>{imdb.get('plot')} </code>", reply_markup=InlineKeyboardMarkup(btn))
+        except Exception as e:
+            print(e)
+            await query.message.reply(f"IMDb Data:\n\n🏷 Title:<a href={imdb['url']}>{imdb.get('title')}</a>\n🎭 Genres: {imdb.get('genres')}\n📆 Year:<a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n🖋 StoryLine: <code>{imdb.get('plot')} </code>", reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
         await query.message.delete()
     else:
-        await query.message.edit(f"IMDb Data:\n\n🏷 Title:<a href={imdb['url']}>{imdb.get('title')}</a>\n🎭 Genres: {imdb.get('genres')}\n📆 Year:<a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n🖋 StoryLine: <code>{imdb.get('plot')} </code>", reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=True)
+        await query.message.edit(f"IMDb Data:\n\n🏷 Title:<a href={imdb['url']}>{imdb.get('title')}</a>\n🎭 Genres: {imdb.get('genres')}\n📆 Year:<a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n🖋 StoryLine: <code>{imdb.get('plot')} </code>", reply_markup=InlineKeyboardMarkup(btn), disable_web_page_preview=False)
     await query.answer()
-        
-
         

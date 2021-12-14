1528d9a72ac0ef1f4b98a359a388da8eb54538d7
Sourcery AI <>
2021-12-14 12:19:51 +0000

    'Refactored by Sourcery'

--- utils.py
+++ utils.py
@@ -97,7 +97,7 @@ async def get_poster(query, bulk=False, id=False):
     else:
         plot = movie.get('plot outline')
     if plot and len(plot) > 800:
-        plot = plot[0:800] + "..."
+        plot = plot[:800] + "..."
     return {
         'title': title,
         'year': date,
@@ -283,8 +283,7 @@ def parser(text, keyword):
         else:
             note_data += text[prev:to_check]
             prev = match.start(1) - 1
-    else:
-        note_data += text[prev:]
+    note_data += text[prev:]
 
     try:
         return note_data, buttons, alerts

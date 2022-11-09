43fa2edf5b046bef794265eb4e67922880af6061
Pooja0033 <113719724+Pooja0033@users.noreply.github.com>
2022-11-09 08:19:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -400,11 +400,21 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ 𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬 ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('☮️𝗖𝗢𝗡𝗧𝗔𝗖𝗧☮️', url='https://t.me/KL_2335')
+            InlineKeyboardButton('⭕️ CHΛИИΞL ⭕️', url='https://t.me/kmtz_channel_v3'),
+
+            InlineKeyboardButton('⭕️ UᎮDΛTΞS ⭕️', url='https://t.me/Prv_35')
+
+            ],[      
+
+            InlineKeyboardButton('🔰 𝗛𝗘𝗟𝗣 🔰', callback_data='help'),
+
+            InlineKeyboardButton('☮️ CONTACT ME ☮️', url='https://t.me/KL_2335'),
+
+            InlineKeyboardButton('⏮️ ΛBOUT ⏯️', callback_data='about')
+
             ],[
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+
+            InlineKeyboardButton('☑️ 𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌  ☑️', url='http://instagram.com/kl_02_pranav_?utm_source=qr')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
